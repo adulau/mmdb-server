38a797cc57ab83de9682aabedba14465aea16705
itsrofly <99050170+itsrofly@users.noreply.github.com>
2026-05-21 18:57:40 +0100

    chore: improve useful logging

--- db/update.sh
+++ db/update.sh
@@ -1,6 +1,29 @@
 #!/bin/sh
 
-wget https://cra.circl.lu/opendata/geo-open/mmdb-country/latest.mmdb
-mv latest.mmdb GeoOpen-Country.mmdb
-wget https://cra.circl.lu/opendata/geo-open/mmdb-country-asn/latest.mmdb
-mv latest.mmdb GeoOpen-Country-ASN.mmdb
+set -u
+
+download_and_replace() {
+  url="$1"
+  target="$2"
+  tmp="${target}.tmp"
+
+  echo "[INFO] Downloading $url -> $target"
+
+  if wget --progress=dot:giga --tries=3 -O "$tmp" "$url"; then
+    echo "[OK] Download successful, replacing $target"
+    mv -f "$tmp" "$target"
+    echo "[OK] Updated $target"
+  else
+    echo "[WARN] Download failed for $url, keeping existing $target"
+    rm -f "$tmp"
+    return 1
+  fi
+}
+
+download_and_replace \
+  "https://cra.circl.lu/opendata/geo-open/mmdb-country/latest.mmdb" \
+  "GeoOpen-Country.mmdb"
+
+download_and_replace \
+  "https://cra.circl.lu/opendata/geo-open/mmdb-country-asn/latest.mmdb" \
+  "GeoOpen-Country-ASN.mmdb"
